1eb9e2320a66ec34d466b4d1bf748cea5624c7af
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-08-11 14:26:03 -0700

    Calculator

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,42 @@
+#!/ussr/bin/python3
+
+def add(a, b):
+    """ Function add
+    Args:
+        a: first number
+        b: sec number
+
+    Return:
+        a + b
+    """
+    return a + b
+
+def sub(a, b):
+    """ Function sub
+    Args:
+        a: first number
+        b: second number
+    Return:
+        a - b
+    """
+    return a - b
+
+def mul(a, b):
+    """ Function mul
+    Args:
+        a: first num
+        b: sec num
+    Return:
+        a * b
+    """
+    return a * b
+
+def div(a, b):
+    """ Function div
+    Args:
+        a: first num
+        b: sec num
+    Return:
+        a * b
+    """
+    return a / b
