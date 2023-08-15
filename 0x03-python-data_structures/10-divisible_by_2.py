4df658e115b7255d4fa0b498217c21cb79162c40
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-08-14 20:16:27 -0700

    Divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new = []
+    new = [0] * len(my_list)
     for i in range(len(my_list)):
         new[i] = my_list[i] % 2 == 0
     return new
