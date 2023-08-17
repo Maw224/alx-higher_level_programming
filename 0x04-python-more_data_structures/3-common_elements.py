4ea0c60e4aadf0740f021cbcf8a5202e40ce2897
Moawia Ahmed <moawia.ahmed97@gmail.com>
2023-08-16 17:14:24 -0700

    Common

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    common = {}
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                common.append(j)
-    return common
+    return set_1 and set_2
